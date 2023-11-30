b410eaa779c8d0aec1181c53d107b6f015126892
Miguelillo el makina <mataramosmiguel@gmail.com>
2023-11-30 17:25:20 +0100

    cosas de migue

---
+++ app/src/main/java/com/example/memorytroncollection/InicioMiguel.kt
@@ -0,0 +1,45 @@
+package com.example.memorytroncollection
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.memorytroncollection.databinding.ActivityInicioMiguelBinding
+import com.example.memorytroncollection.databinding.ActivityMainBinding
+
+class InicioMiguel : AppCompatActivity() {
+    private lateinit var bind : ActivityInicioMiguelBinding
+    private var cont = 0
+    private var oculto = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bind = ActivityInicioMiguelBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+        bind.jugar.setOnClickListener {
+            var intent = Intent(this,JuegoMiguel::class.java)
+            intent.putExtra("oculto",oculto)
+            startActivity(intent)
+        }
+        bind.jugar2.setOnClickListener {
+            var intent = Intent(this,JuegoMiguel::class.java)
+            intent.putExtra("oculto",oculto)
+            startActivity(intent)
+        }
+
+        bind.card.setOnClickListener {
+            cont ++
+            if (cont > 5){
+                bind.jugar.visibility = View.INVISIBLE
+                bind.jugar2.visibility = View.VISIBLE
+                bind.t.text = getString(R.string.msj_bienvenidaR)
+                bind.t.textSize = 15f
+                bind.explicacion.text = getString(R.string.explicacionR)
+                bind.explicacion.textSize = 15f
+                oculto = true
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/memorytroncollection/JuegoMiguel.kt
@@ -0,0 +1,11 @@
+package com.example.memorytroncollection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JuegoMiguel : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_juego_miguel)
+    }
+}
