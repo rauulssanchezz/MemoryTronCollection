30d4f3e2af843b6b8c3a9f3b0c4fecebe3237419
Miguelillo el makina <mataramosmiguel@gmail.com>
2023-11-30 18:05:47 +0100

    cozitas

--- app/src/main/java/com/example/memorytroncollection/Ganador_Pablo.kt
+++ app/src/main/java/com/example/memorytroncollection/Ganador_Pablo.kt
@@ -19,13 +19,13 @@ class Ganador_Pablo : AppCompatActivity() {
     }
     fun Reiniciar(view: View) {
         mediaPlayer?.stop()
-        val intent= Intent(this, Juego::class.java)
+        val intent= Intent(this, JuegoPablo::class.java)
         startActivity(intent)
     }
 
     override fun onBackPressed() {
         mediaPlayer?.stop()
-        val intent= Intent(this, Juego::class.java)
+        val intent= Intent(this, JuegoPablo::class.java)
         startActivity(intent)
     }
 
